7aa2aa72b02ea635599ea6cd300eb632db7eed13
lingcheng99 <lingcheng99@gmail.com>
2015-10-21 12:33:25 -0700

    Update randomforest.py

--- randomforest.py
+++ randomforest.py
@@ -7,12 +7,20 @@ from sklearn.ensemble import RandomForestClassifier
 data=pd.read_csv('train.csv')
 data1=data.values
 X=data1[:,1:]
+y=data1[:,:1]
 y=np.ravel(y)
 Xtrain,Xtest,ytrain,ytest=cross_validation.train_test_split(X,y,test_size=0.25)
+
+#Display the digit image
+import matplotlib.pyplot as plt
+plt.rc("image", cmap="binary")
+plt.imshow(X[1,].reshape(28,28))
 
 #Run RandomForestClassifier
 rf=RandomForestClassifier()
 rf.fit(Xtrain,ytrain)
 ypred=rf.predict(Xtest) 
-metrics.precision_score(ytest,ypred)
+metrics.precision_score(ytest,ypred,average='weighted')
+Out[33]: 0.93764982780567763
+
 
